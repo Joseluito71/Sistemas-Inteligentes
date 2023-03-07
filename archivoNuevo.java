6c8ef0a0149d5446b57f7e27c7d6845c9c8479f7
Joseluito71 <110116614+Joseluito71@users.noreply.github.com>
2023-03-07 11:53:00 +0100

    Update archivoNuevo.java

--- archivoNuevo.java
+++ archivoNuevo.java
@@ -155,4 +155,83 @@ Footer navigation
     Blog
     About
 
-AlgoritmoA/AlgoritmoAEstrela.java at master · lucasmartioli/AlgoritmoA
+/*package practica2021a;
+
+import practica2021a.DataStructures.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @authos L. Mandow
+ * @date   11/marzo/2021
+ * Agente resolutor de problemas usando A*.
+ */
+
+public class AgenteA{ 
+	
+	/**
+	 * Resuelve un problema dado el estado inicial y el objetivo.
+	 * 
+	 */
+
+	public List<Estado> resuelve(Estado salida, Estado objetivo){
+
+		Arbol<Estado> arbol = new ArbolHashMap<>();
+		Abiertos<Estado> abiertos = new AbiertosList<>();
+
+		Estado e = salida;
+		Nodo<Estado> n = new Nodo<>(e, 0, null);
+		arbol.put(n);
+		abiertos.offer(e.h(objetivo), e);
+		
+		while (!abiertos.isEmpty()){
+			e = abiertos.poll();
+			n = arbol.get(e);
+			
+			if(e.equals(objetivo)){     //si es el objetivo, terminamos
+				return recuperaSolucion(n, arbol);
+			} else { //generamos los sucesores no repetidos y los a�adimos al �rbol y a abiertos
+				int ge = n.getG();
+				for(Estado e2: e.calculaSucesores()){
+					int nuevoCoste = ge + e.coste(e2);
+					
+					if (!arbol.containsKey(e2)){	//nuevon nodo: simplemente a�adimos e2 al �rboly a abiertos
+						arbol.put(new Nodo<>(e2, nuevoCoste, n));
+						abiertos.offer(nuevoCoste + e2.h(objetivo),  e2);
+					} else {
+						Nodo<Estado> n2 = arbol.get(e2);
+						int antiguoCoste = n2.getG();
+						if (nuevoCoste < antiguoCoste) { // mejor camino: redirigimos el puntero de e2 y actualizamos abiertos
+							n2.setPadre(n);
+							n2.setG(nuevoCoste);
+							
+							abiertos.remove(e2);
+							abiertos.offer(nuevoCoste + e2.h(objetivo), e2);
+						}
+					}
+				
+				}//for e2
+			}//if-else
+		}//while
+		
+		return null;		 //la búsqueda termina con fracaso
+	}
+	
+	ArrayList<Estado> recuperaSolucion(Nodo<Estado> n, Arbol<Estado> arbol){
+		ArrayList<Estado> solucion = new ArrayList<Estado>();
+		
+		Nodo<Estado> n2 = n;
+		
+		while(n2  != null){
+			solucion.add(n2.getEstado());
+			n2 = n2.getPadre();
+		}//while
+		
+		return solucion;
+
+	}
+} */

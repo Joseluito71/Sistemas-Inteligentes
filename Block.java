60959361791a67cef0c8de678e19857750bb9156
IsraelGomezUrbano <israelgomez2003@uma.es>
2023-03-14 12:00:44 +0100

    Creación de bloques
    Creado los bloques
    
    Co-Authored-By: JuanjoRC1 <125024911+JuanjoRC1@users.noreply.github.com>

---
+++ Block.java
@@ -0,0 +1,10 @@
+public class Block {
+    String valor;
+    int x,y;
+    public Block(int a, int b,String v){
+        this.x=a;
+        this.y=b;
+        this.valor=v; // Puede ser "I","G", " ", "*","+"
+    }
+    
+}
